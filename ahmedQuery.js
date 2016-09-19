74df4033763feb7a0504f07a3146e7b7d356f957
Ahmed Elnoamany <ahmedgouda9484@gmail.com>
2016-09-19 18:20:15 -0500

    added Querying for elements.

--- ahmedQuery.js
+++ ahmedQuery.js
@@ -6,6 +6,18 @@
     }
 }
 
+if(typeof selector !== 'string'){  //returns the ahmedQuery object if selector is not a string.
+  return this;
+}
+//Querying for elements
+var nodes = document.querySelectorAll(selector); //stores list of all elements matching query in 'nodes'
+javascript nodes.forEach(function(value, index){
+  this[index] = value;
+}, this);  //'this' refers to ahmedQuery object
+this.length = nodes.length;
+
+return this;
+
 window.ahmedQuery = ahmedQuery;
 window.$ = ahmedQuery;
 })()
